e15cd167b5b97ae6fb2eec0dc9fdc6891d89dff3
StuffGames <44318828+StuffGames@users.noreply.github.com>
2019-04-22 10:51:43 -0400

    Base Game
    Basically the base game and the start. Need to work on movement.

---
+++ Snake Game/Assets/SnakeMovementScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovementScript : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Transform tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        StartCoroutine(Movement());
+    }
+
+    public IEnumerator Movement()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y + 0.25f);//WHAT IS HAPPENING????!?!?!
+        yield return new WaitForSeconds(1);
+    }
+
+}
